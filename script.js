6eafbd430cb7d9c40bc77292744cf44da8ad8333
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-05-27 18:02:00 +0300

    Add UI Functionality - display books and add book

--- script.js
+++ script.js
@@ -8,6 +8,28 @@ class Book {
     };
 }
 
+class UI {
+    static displayBooks() {
+        let books;
+
+        // books = Store.getBooks();
+
+        books.forEach(book => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${book.title}</td>
+                         <td>${book.author}</td>
+                         <td>${book.isbn}</td>
+                         <td><a href="#" class="delete">X</a></td>`;
+
+        list.appendChild(row);
+    }
+}
+
 // UI Functionality - display books, add books, remove books , clearForm, showAlerts
 
 // Events - submit book, remove book, display books
