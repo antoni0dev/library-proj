38cbbad502fc2bde12200682486f185661d784ac
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-05-27 17:50:58 +0300

    Add Book Class

---
+++ script.js
@@ -0,0 +1,13 @@
+// Book Class - used to instantiate books
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    };
+}
+
+// UI Functionality - display books, add books, remove books , clearForm, showAlerts
+
+// Events - submit book, remove book, display books
